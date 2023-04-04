299b88e556526a6a2627fe82bd0ce7699715a66b
Gray-Desktop <gray.boulware@gmail.com>
2023-04-04 12:09:47 -0500

    btn update

--- src/components/personCard.js
+++ src/components/personCard.js
@@ -5,17 +5,21 @@ class PersonCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            age: this.props.age
+            currentAge: this.props.age
         }
     }
 
+    incAge = () => this.setState({ 
+        currentAge: this.state.currentAge + 1
+    })
+
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return <div>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {this.state.age}</p>
+            <p>Age: {this.state.currentAge}</p>
             <p>Hair Color: {hairColor}</p>
-            <button onClick={() => this.setState({ age: age + 1})}>Birthday Button for {firstName} {lastName}</button>
+            <button onClick={this.incAge}>Birthday Button for {firstName} {lastName}</button>
         </div>
     }
 }
